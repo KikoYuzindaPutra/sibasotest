ca2788a98fca282961ca0798c9941896c077f30c
ClarenceFer <ferdinandclarence5@gmail.com>
2025-11-04 20:37:09 +0700

    Memperbaiki CORS Patch

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const corsOptions = {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS"],
   allowedHeaders: ["Authorization", "x-access-token", "Origin", "Content-Type", "Accept"],
   credentials: true
 };
